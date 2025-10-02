94709bf9c9270dd34ccd503bdd372bc769989b8b
Ying-Ju Chen <yche1364@asu.edu>
2025-10-02 11:26:34 -0700

    Update portmap.sh

--- https/portmap.sh
+++ https/portmap.sh
@@ -1,1 +1,2 @@
-sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 1234
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 8080
+sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to 8443
