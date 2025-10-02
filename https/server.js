30f930b585b2ff2a9b71b7bb21e2981a72021210
Yingju-c <yche1364@asu.edu>
2025-10-02 11:06:38 -0700

    https

---
+++ https/server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var server = express();
+var bodyParser = require('body-parser');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 1234;
+
+server.get("/", function (req, res) {
+    res.redirect("index.html")
+});
+
+
+server.use(bodyParser());
+server.use(express.static(__dirname + '/public'));
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+server.listen(port);
+
+//server.listen(port);
+
+if (require.main === module) { server.listen(port); }// Instead do export the app:
+else{ module.exports = server; }
